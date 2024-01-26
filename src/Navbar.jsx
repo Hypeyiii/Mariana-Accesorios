6404887067539d0358497c5debd4191124672f9e
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-01-26 12:49:26 -0600

    change loggin modal

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,57 +3,67 @@ import userLogo from './assets/userLogo.svg';
 import heartLogo from './assets/heartLogo.svg';
 import shoppingCartLogo from './assets/shoppingCartLogo.svg';
 import { Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from 'flowbite-react';
+import { useState } from 'react';
 import './Menu.css';
 
 export function Header(){
-
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  
+  const handleClickOpen = () => {
+      setNavbarOpen(true);
+  }
+  const handleClickClose = () => {
+      setNavbarOpen(useState);
+  }
+  
     return(
       <>
-        <Navbar className='fixed z-40 top-0  shadow-md flex flex-col sm:flex-row px-8 backdrop-blur-sm bg-white/70 md:bg-white/60 transition-all duration-1000 w-full mx-auto'>
-      <div className='flex justify-start items-center mt-2 mb-2 mr-auto sm:mx-auto sm:w-full '
-      id='inicio'>
-        <a href='#' className='mr-auto sm:mx-auto'>
-          <img src={logo} alt="Logo" className='w-28 h-16 cursor-pointer' />
-        </a>
-      </div>
-      <NavbarBrand>
-        <NavbarToggle/>
-      </NavbarBrand>
-      <NavbarCollapse className='menu-animated mx-auto h-screen w-full sm:h-auto'>
-        <div className='grow flex flex-col sm:flex-row gap-x-12 text-lg md:text-xl text-black/80 justify-center 
-        items-start sm:items-center mx-auto [&>a]:transition-all [&>a]:duration-300
-        py-4 md:py-0 w-[125px] sm:w-auto'>
-          <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Inicio</a>
-          <a href="/#lanzamientos" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Lanzamientos</a>
-          <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Anillos</a>
-          <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Aretes</a>
-          <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Collares</a>
-        </div>
-        <div className='flex flex-col sm:flex-row list-none justify-center md:justify-end items-start md:items-center md:gap-x-10 
-            gap-y-3 mx-auto w-[125px] sm:w-auto'>
-          <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
-                transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
-            <a href="#" className='flex flex-row items-center justify-center'>
-              <img src={userLogo} alt="User Logo" className='size-6 duration-300' />
-                <span className='font-light text-sm'>
-                Cuenta</span>
-            </a>
+        <Navbar className='fixed z-40 top-0  shadow-md flex flex-col sm:flex-row px-8 
+          backdrop-blur-sm bg-white/70 md:bg-white/60 transition-all duration-1000 w-full mx-auto'>
+          <div className='flex justify-start items-center mt-2 mb-2 mr-auto sm:mx-auto sm:w-full'
+                id='inicio'>
+              <a href='#' className='mr-auto sm:mx-auto'>
+                <img src={logo} alt="Logo" className='w-28 h-16 cursor-pointer' />
+              </a>
           </div>
-          <a href="#" className='flex flex-row items-center justify-center'>
-            <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
-            transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
-              <img src={heartLogo} alt="User Logo" className='size-6  duration-300' />
-              <span className='font-light text-sm'>Favoritos</span>
+        <NavbarBrand>
+          <NavbarToggle/>
+        </NavbarBrand>
+        <NavbarCollapse className='menu-animated mx-auto h-screen w-full sm:h-auto'>
+            <div className='grow flex flex-col sm:flex-row gap-x-12 text-lg md:text-xl text-black/80 justify-center 
+            items-start sm:items-center mx-auto [&>a]:transition-all [&>a]:duration-300
+            py-4 md:py-0 w-[125px] sm:w-auto'>
+              <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Inicio</a>
+              <a href="/#lanzamientos" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Lanzamientos</a>
+              <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Anillos</a>
+              <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Aretes</a>
+              <a href="#" className='hover:border-b-[#fad2f6] border-b-2 border-b-transparent'>Collares</a>
             </div>
-          </a>
-          <a href="#" className='flex flex-row items-center justify-center'>
-            <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
-            transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
-              <img src={shoppingCartLogo} alt="User Logo" className='size-6  duration-300' />
-              <span className='font-light text-sm'>Carrito</span>
+            <div className='flex flex-col sm:flex-row list-none justify-center md:justify-end items-start md:items-center md:gap-x-10 
+                gap-y-3 mx-auto w-[125px] sm:w-auto'>
+              <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
+                    transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
+                <a href="#" className='flex flex-row items-center justify-center'>
+                  <img src={userLogo} alt="User Logo" className='size-6 duration-300' />
+                    <span className='font-light text-sm'>
+                    Cuenta</span>
+                </a>
+              </div>
+              <a href="#" className='flex flex-row items-center justify-center'>
+                <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
+                transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
+                  <img src={heartLogo} alt="User Logo" className='size-6  duration-300' />
+                  <span className='font-light text-sm'>Favoritos</span>
+                </div>
+              </a>
+              <a href="#" className='flex flex-row items-center justify-center'>
+                <div className='flex items-center justify-center flex-row gap-x-1 cursor-pointer
+                transition-all duration-300 border-transparent border-b-2 hover:border-b-pink-300/60'>
+                  <img src={shoppingCartLogo} alt="User Logo" className='size-6  duration-300' />
+                  <span className='font-light text-sm'>Carrito</span>
+                </div>
+              </a>
             </div>
-          </a>
-        </div>
       </NavbarCollapse>
     </Navbar>
     </>
--- src/hooks/LogginModal.jsx
+++ src/hooks/LogginModal.jsx
@@ -1,11 +1,10 @@
-import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 import { useState } from 'react';
-import {Modal} from 'flowbite-react';
+import closeIcon from '../assets/close.svg';
+import userLogo from '../assets/userLogo.svg'; 
 
 function LogginModal(){
 
     const [modalOpen, setModalOpen] = useState(false);
-    const [email, setEmail] = useState('');
 
     const handleOpen=()=>{
         setModalOpen(true);
@@ -13,7 +12,6 @@ function LogginModal(){
 
     const handleClose=()=>{
         setModalOpen(false);
-        setEmail('');
     };
 
     return(
@@ -24,51 +22,66 @@ function LogginModal(){
                    {modalOpen ? "Registrarse" : "Registrarse"}
                 </button>,
                 {modalOpen && (
-        <>
-        <Modal size="sm" show popup>
-          <Modal.Header onClick={handleClose}/>
-          <Modal.Body>
-            <div className="space-y-6">
-              <h3 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="email" value="Your email" />
-                </div>
-                <TextInput
-                  id="email"
-                  placeholder="name@company.com"
-                  value={email}
-                  onChange={(event) => setEmail(event.target.value)}
-                  required
+      <>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="fixed inset-0 bg-black opacity-50" onClick={handleClose}></div>
+          <div className="bg-white p-2 rounded-md z-10 shadow-xl shadow-gray-600">
+          <div className='flex items-end justify-end'>
+            <img src={closeIcon} className='size-6 cursor-pointer hover:bg-gray-200 transition-all duration-300' onClick={handleClose} alt="" />
+            </div>
+            <div className='p-8 flex flex-col items-start jusitfy-start gap-y-4'>
+            <h2 className="text-2xl font-bold mb-4">Sing it to our plataform</h2>
+            <form>
+              <div className="mb-4">
+                <label className="block text-gray-600">Your email</label>
+                <input
+                  placeholder='name@company.com'
+                  type="text"
+                  className="w-full p-2 border rounded-md"
                 />
               </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="password" value="Your password" />
+              <div className="mb-8">
+                <label className="block text-gray-600">Your password</label>
+                <input
+                  type="password"
+                  className="w-full p-2 border rounded-md"
+                />
+              </div>
+              <div className='flex flex-row justify-between text-sm'>
+                <div className='flex flex-row gap-x-2 items-center justify-center'>
+                <input 
+                type="checkbox" 
+                id='remember' 
+                className='rounded-md checked:bg-pink-300/60
+                focus:ring-0 shadow-sm shadow-black' />
+                <label htmlFor='remember' className='cursor-pointer'>Remember me</label>
                 </div>
-                <TextInput id="password" type="password" required />
-              </div>
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <Checkbox id="remember" />
-                  <Label htmlFor="remember">Remember me</Label>
+                <div className='flex hover:underline cursor-pointer justify-end items-end'>
+                  <h1>Lost Password?</h1>
                 </div>
-                <a href="#" className="text-sm text-cyan-700 hover:underline dark:text-cyan-500">
-                  Lost Password?
-                </a>
               </div>
-              <div className="w-full">
-                <Button>Log in to your account</Button>
+              <div className="flex justify-start mt-8">
+                <button
+                  type="button"
+                  className="p-2 bg-black/90 hover:bg-pink-300/60 hover:text-black text-white rounded-md
+                  transition-all duration-300 shadow-lg shadow-black font-bold"
+                  onClick={handleClose}
+                >
+                  Iniciar Sesión
+                </button>
               </div>
-              <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered?&nbsp;
-                <a href="#" className="text-cyan-700 hover:underline dark:text-cyan-500">
-                  Create account
-                </a>
+              <div className='flex flex-row justify-between mt-8 text-sm'>
+                  <div className='flex items-start justify-start'>
+                    <h1 className='text-black/60'>Not register yet?</h1>
+                  </div>
+                  <div>
+                  <h1 className='hover:underline cursor-pointer'>Create account</h1>
+                  </div>
               </div>
-            </div>
-          </Modal.Body>
-        </Modal>
+            </form>
+          </div>
+        </div>
+        </div>
       </>
       )}
       </>

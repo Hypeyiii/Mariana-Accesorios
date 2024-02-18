cee0c4a63fa4f0cab0caf98a2ba2d17d1cdb3fae
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-02-17 18:33:54 -0600

    Added last name input and added confetti when pays

--- src/Components/CheckoutForm.jsx
+++ src/Components/CheckoutForm.jsx
@@ -1,37 +1,29 @@
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { FaArrowRight, FaStripeS } from "react-icons/fa6";
+import { useState } from "react";
+import { FaArrowRight, FaStripe, FaStripeS } from "react-icons/fa6";
 import "../hooks/Info.css";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { CiCircleCheck } from "react-icons/ci";
+import Confetti from "react-confetti";
 
 const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
   const [paymentSuccess, setPaymentSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
-  useEffect(() => {
-    let timeout;
-    if (paymentSuccess) {
-      timeout = setTimeout(() => {
-        setPaymentSuccess(false);
-      }, 4000);
-    }
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [paymentSuccess]);
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const { name, email, phoneNumber } = e.target.elements;
+    const { name, email, phoneNumber, lastName } = e.target.elements;
     if (
       !stripe ||
       !elements ||
       !name.value ||
       !email.value ||
-      !phoneNumber.value
+      !phoneNumber.value ||
+      !lastName.value
     ) {
       return;
     }
@@ -46,10 +38,12 @@ const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
         amount: parseInt(price) * 100,
         email: email.value,
         name: name.value,
+        lastName: lastName.value,
         phoneNumber: phoneNumber.value,
         allProducts: allProducts
           .map((product) => product.productName)
           .join(", "),
+        customerId: email.value,
       });
       setPaymentSuccess(true);
       console.log(data);
@@ -63,7 +57,7 @@ const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
         className="fixed top-0 z-50 w-screen h-screen bg-black/70 m-auto"
         onClick={onBuy}
       ></div>
-      <div className="scale-in-center z-50 fixed bg-white top-0 bottom-0 right-0 mx-auto left-0 my-auto w-screen h-screen md:w-[70%] md:h-[80%] md:rounded-[30px] overflow-y-auto">
+      <div className="scale-in-center z-50 fixed bg-white top-0 bottom-0 right-0 mx-auto left-0 my-auto w-screen h-screen md:w-[70%] md:h-[80%] md:rounded-[30px] overflow-auto md:overflow-hidden">
         <div
           className="absolute top-0 right-0 p-5 text-pink-300 cursor-pointer text-2xl hover:text-red-500 z-50"
           onClick={onBack}
@@ -77,15 +71,28 @@ const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
           >
             <div className="absolute top-0 left-0 md:left-auto md:right-0 p-5">
               <div className="p-1 text-white bg-purple-500 rounded-lg group/item">
-                <FaStripeS />
+                <a
+                  target="_blank"
+                  rel="noopener noreference"
+                  href="https://stripe.com/es"
+                >
+                  <FaStripeS />
+                </a>
                 <div className="invisible group-hover/item:visible absolute text-white bottom-4 left-12 text-xs w-fit p-2 text-center bg-black/50 mx-auto">
                   ir a stripe.com
                 </div>
               </div>
             </div>
             <div className="flex flex-col justify-between items-center md:text-pretty gap-y-8 mt-10 md:mt-0">
-              <p className="text-xl md:text-3xl text-black font-bold w-full">
-                Nuestros pagos y transacciones son procesados y asegurados por{" "}
+              <p className="text-xl md:text-3xl text-black font-semibold w-full">
+                Los pagos y transacciones en{" "}
+                <span
+                  className="text-pink-300 animate-text-gradient bg-clip-text text-transparent 
+                bg-gradient-to-r from-[#f0c4ff] via-[#c533ee] to-[#f9b3ff] bg-[200%_auto]"
+                >
+                  Mariana Accesorios
+                </span>{" "}
+                son procesados y asegurados por{" "}
                 <a
                   href="https://stripe.com/es"
                   target="_blank"
@@ -100,12 +107,36 @@ const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
                 alt="Shopping online image"
               />
             </div>
+            <ul className="absolute bottom-0 md:mb-2 text-black/70 flex flex-row gap-x-6 text-[10px] items-center justify-center mx-auto">
+              <a
+                href="https://stripe.com/es"
+                target="_blank"
+                rel="noopener noreference"
+                className="flex flex-row gap-x-1 items-center justify-center"
+              >
+                Powered by
+                <FaStripe className="text-black size-6" />
+              </a>
+              <a href="#">Privacidad</a>
+              <a href="#">Términos</a>
+            </ul>
           </div>
           <div className="text-base flex flex-col items-center justify-center col-span-12 md:col-span-8 mx-auto gap-10 relative w-full">
             {paymentSuccess ? (
-              <div className="slide-in-blurred-top text-2xl font-bold text-green-500">
-                ¡Pago realizado exitosamente!
-              </div>
+              <>
+                <Confetti
+                  width={window.innerWidth}
+                  height={window.innerHeight}
+                />
+                <div className="slide-in-blurred-top text-2xl font-bold text-green-500 flex flex-col gap-y-4 items-center justify-center px-20 text-center">
+                  <CiCircleCheck className="size-10 text-green-500" />
+                  <p>¡Pago realizado exitosamente!</p>
+                  <p className="text-lg font-thin">
+                    Gracias por compar en Mariana Accesorios, te enviaremos un
+                    correro con la información de tu recibo.
+                  </p>
+                </div>
+              </>
             ) : (
               <>
                 <h1 className="text-base md:text-2xl font-semibold">
@@ -133,6 +164,7 @@ const CheckoutForm = ({ price, onBuy, onBack, allProducts }) => {
                       </p>
                       <input
                         type="text"
+                        name="lastName"
                         placeholder=""
                         className="rounded-lg border-black/30 shadow-black/20 shadow-lg"
                       />
